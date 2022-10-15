9abf66e7fee4e9807584649a90690fd99241b708
Payam Qorbanpour <payamqorbanpour@gmail.com>
2022-10-15 15:48:20 +0330

    Fix region name

--- pkg/paas/migration.go
+++ pkg/paas/migration.go
@@ -18,24 +18,17 @@ import (
 
 const (
 	checkmark = "[\u2713]"
-	at1       = "ir-thr-ba1"
+	xmark     = "[x]"
+	ba1       = "ir-thr-ba1"
 )
 
 // NewCmdMigrate returns new cobra commad enables user to migrate projects to another region on arvan servers
 func NewCmdMigrate(in io.Reader, out, errout io.Writer) *cobra.Command {
-	// Main command
 	cmd := &cobra.Command{
 		Use:   "migrate",
 		Short: "Migrate namespaces to destination region",
 		Long:  loginLong,
 		Run: func(c *cobra.Command, args []string) {
-			/* TODO
-			check current region(if asiatech migration is acceptable)
-			select projects/namespaces to migrate
-			select destination region2
-			migration proccess
-			output(if failed why and what to do/ if success message)
-			*/
 			explainOut := term.NewResponsiveWriter(out)
 			c.SetOutput(explainOut)
 
@@ -44,7 +37,7 @@ func NewCmdMigrate(in io.Reader, out, errout io.Writer) *cobra.Command {
 
 			currentRegionName := getCurrentRegion()
 
-			if currentRegionName == at1 {
+			if currentRegionName == ba1 {
 				log.Printf("migration from region %s is not possible now\nplease first switch your region using command:\n\n \tarvan paas region\n\n", currentRegionName)
 				return
 			}
@@ -142,8 +135,8 @@ func sprintProjects(projects []string) string {
 func migrationSteps() {
 	fmt.Print("- Getting all resources")
 	time.Sleep(2 * time.Second)
-	fmt.Println(" ", checkmark)
-	fmt.Print("- Step 2")
+	fmt.Println(" ", xmark)
+	fmt.Print("- Stopping user services")
 	time.Sleep(2 * time.Second)
 	fmt.Println(" ", checkmark)
 	fmt.Print("- Transfering volumes to new region")
